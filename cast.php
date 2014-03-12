1e2e491510fecce120a9ac424a51aaf7aff8becf
Tony Tran <toant713@gmail.com>
2014-03-11 20:45:26 -0700

    updated cast

--- cast.php
+++ cast.php
@@ -59,34 +59,42 @@
        
         	<hr class="doublestyle" />
         	<h1>CAST</h1>
-            <hr class="doublestyle" />
-            
-           
-           	<h2>Martin O'Leary: 28-33. </h2>
-            <p>
-            He is a hardworking, smart and calculative young man, and as a result he has become a valuable asset at the real estate firm that he works for.  He has changed everything about himself to fit the part, but it bothers him that he still looks young for his age.  Martin always keeps moving forward and never looks back. His lack of nostalgia stems from his estranged relationship with his father, Michael O'Leary, whom he hasn't spoken with for nearly 10 years.  After learning that his father has passed away, he must now face his fear of returning to his small Irish hometown.  His plan is sell his father's pub and continue working to prove that he is more than just a son of a barman. 
-            </p>
-            
-            <h2>Jay Coakley: 58-63 </h2>
-            <p>
-           Jay has kind eyes and a strong Irish brogue.  The wrinkles and gray stubble on his face suggest he has seen better days.  He is the lifelong best friend of the late Michael O'Leary.  Michael was there for Jay when Mrs. Coakley left him, and Jay was there for Michael when his wife past away.  Jay is a loyal friend and does what he can to help out.  Although soft spoken, his words are well chosen.  Jay wouldn't hurt a fly, but could put you in your place if he had to. 
-            </p>
-            
-            <h2>Mr. Snyder: 50-55</h2>
-            <p>
-Snyder isn't just Martin's boss; he is also his role model. Smart, quick, successful and not to mention looks sharp in a tailored suit - Snyder appears to have it all in order. While he may be cunning and devilishly charming, his allegiance is always to himself and he will do what he has to in order to get the job done. He is someone you want on your side when the proverbial gloves come off.
-			</p>
-
-			<h2>Mrs. Kirby: 70-75</h2>
-            <p>
-Kathy Kirby has always been young at heart.  She is a prim and proper Irish lady, but also has a taste for whiskey.  Mrs. Kirby is kind and caring, but can't quite manage by herself since her husband passed away.  She is missing a few screws, but somehow is in tune with what is going on around her.  It's Mrs. Kirby's quick wit and occasional gems of wisdom that make people question whether she really is a bit batty or if she is just feigning.  
-</p>
-
-			<h2>Murph and Arnie: 40-45</h2>
-            <p>
-Murph and Arnie are an Irish Tweedle-dee and Tweedle-dum.  They are the best of friends, but that doesn't mean they haven't thrown a few punches at each other.  Arnie is a bigger fella with a boisterous voice.  He always seems to bring the party with him.  Murph, though a bit smaller than Arnie, is the one to keep them out of trouble.  With a straight face, Murph can make a whole room roll on the floor laughing. They are two hell raisers with hearts of gold.  If they aren't making fun of you, it means they don't like you.  
-			</p>
-
+           <hr class="doublestyle" />
+           <div class="profile">
+           	<img src="img/Martin_OLeary.jpg"/>
+            	<h2>Martin O'Leary <a target="_blank" href="http://www.imdb.com/name/nm0321733/?ref_=tt_cl_t10">[Brian Glanney]</a></h2>
+            	<p>
+            	He is a hardworking, smart and calculative young man, and as a result he has become a valuable asset at the real estate firm that he works for.  He has changed everything about himself to fit the part, but it bothers him that he still looks young for his age.  Martin always keeps moving forward and never looks back. His lack of nostalgia stems from his estranged relationship with his father, Michael O'Leary, whom he hasn't spoken with for nearly 10 years.  After learning that his father has passed away, he must now face his fear of returning to his small Irish hometown.  His plan is sell his father's pub and continue working to prove that he is more than just a son of a barman. 
+            	</p>
+           </div>
+           <div class="profile">
+           	<img src="img/Jay_Coakley.jpg"/>
+                <h2>Jay Coakley <a target="_blank" href="http://www.imdb.com/name/nm0790474/?ref_=tt_cl_t5">[Alan Shearman]</a></h2>
+                <p>
+               Jay has kind eyes and a strong Irish brogue.  The wrinkles and gray stubble on his face suggest he has seen better days.  He is the lifelong best friend of the late Michael O'Leary.  Michael was there for Jay when Mrs. Coakley left him, and Jay was there for Michael when his wife past away.  Jay is a loyal friend and does what he can to help out.  Although soft spoken, his words are well chosen.  Jay wouldn't hurt a fly, but could put you in your place if he had to. 
+                </p>
+            </div>
+           <div class="profile">
+           	<img src="img/Bill_Snyder.jpg"/>
+                <h2>Bill Snyder <a target="_blank" href="http://www.imdb.com/name/nm0876138/?ref_=tt_cl_t1">[Alan Tudyk]</a></h2>
+                <p>
+    Snyder isn't just Martin's boss; he is also his role model. Smart, quick, successful and not to mention looks sharp in a tailored suit - Snyder appears to have it all in order. While he may be cunning and devilishly charming, his allegiance is always to himself and he will do what he has to in order to get the job done. He is someone you want on your side when the proverbial gloves come off.
+                </p>
+            </div>
+           <div class="profile">
+           	<img src="img/Kathy_Kirby.jpg"/>
+                <h2>Kathy Kirby <a target="_blank" href="http://www.imdb.com/name/nm0311619/?ref_=tt_cl_t2">[Elen Geer]</a></h2></h2>
+                <p>
+    Kathy Kirby has always been young at heart.  She is a prim and proper Irish lady, but also has a taste for whiskey.  Mrs. Kirby is kind and caring, but can't quite manage by herself since her husband passed away.  She is missing a few screws, but somehow is in tune with what is going on around her.  It's Mrs. Kirby's quick wit and occasional gems of wisdom that make people question whether she really is a bit batty or if she is just feigning.  
+    </p>
+			</div>
+           <div class="profile" style="border:none;">
+           	<img src="img/Arnie.jpg"/>
+                <h2>Murph and Arnie <a target="_blank" href="http://www.imdb.com/name/nm0346886/?ref_=tt_cl_t6">[Darrel Guilbeau</a> and<a target="_blank" href="http://www.imdb.com/name/nm1107847/?ref_=tt_cl_t3"> Steve Humphreys]</a></h2></h2>
+                <p>
+    Murph and Arnie are an Irish Tweedle-dee and Tweedle-dum.  They are the best of friends, but that doesn't mean they haven't thrown a few punches at each other.  Arnie is a bigger fella with a boisterous voice.  He always seems to bring the party with him.  Murph, though a bit smaller than Arnie, is the one to keep them out of trouble.  With a straight face, Murph can make a whole room roll on the floor laughing. They are two hell raisers with hearts of gold.  If they aren't making fun of you, it means they don't like you.  
+                </p>
+			</div>
            
            
         </div><!--inner-->
